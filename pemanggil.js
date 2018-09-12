1626bb21a4e38271142b91b87e5a809f6cfad824
sutarsaerik <42233431+sutarsaerik@users.noreply.github.com>
2018-09-12 17:27:12 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,12 @@ if(x=="1"){xxx="/2018/09/bitcoin-ban-expands-across-credit-cards.html"}
 if(x=="2"){xxx="/2018/09/an-insurance-maze-for-us-doctors.html"}
 if(x=="3"){xxx="/2018/09/new-life-insurance-code-riddled-with.html"}
 if(x=="4"){xxx="/2018/09/bitcoin-fever-hits-us-real-estate-market.html"}
-if(x=="5"){xxx="/2018/09/bitcoin-pop-culture-moment-fades-as-fad.html"}a_to_ve[i].href="https://vlikoo-apk-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/09/bitcoin-pop-culture-moment-fades-as-fad.html"}
+if(x=="6"){xxx="/2018/09/new-life-insurance-code-riddled-with_12.html"}
+if(x=="7"){xxx="/2018/09/australians-can-be-denied-life.html"}
+if(x=="8"){xxx="/2018/09/comminsure-case-shows-its-time-to.html"}
+if(x=="9"){xxx="/2018/09/mesothelioma-law-resources.html"}
+if(x=="10"){xxx="/2018/09/the-benefits-of-your-car-donation-in.html"}a_to_ve[i].href="https://vlikoo-apk-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
